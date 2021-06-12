2aa0c9eafc31384e0bcc60382a3ac3f022723414
meanagha <iamanaghapatil@gmail.com>
2021-06-12 17:12:50 +0530

    2 commit

--- src/index.server.js
+++ src/index.server.js
@@ -22,9 +22,22 @@ env.config();
 
 // mongodb connection
 //mongodb+srv://root:<password>@cluster0.8pl1w.mongodb.net/<dbname>?retryWrites=true&w=majority
+// mongoose
+//   .connect(
+//     `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.8pl1w.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+//     {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       useCreateIndex: true,
+//       useFindAndModify: false,
+//     }
+//   )
+//   .then(() => {
+//     console.log("Database connected");
+//   });
+var url = "mongodb://localhost:27017/EC1";
 mongoose
-  .connect(
-    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.8pl1w.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+  .connect(url,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
